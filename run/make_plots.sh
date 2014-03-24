d5a2958aac4e63bcb5cb60fa908b7ae0f9c81bcb
Andy Aschwanden <andy.aschwanden@gmail.com>
2014-03-23 19:59:12 -0800

    work on plot script

--- run/make_plots.sh
+++ run/make_plots.sh
@@ -9,13 +9,17 @@ for q in 0.1 0.25; do
 	for mu in 1e-5 5e-5 1e-6 5e-6; do
 	    for omega in 1 10 100 1000 10000; do
 		filepre=g${GRID}km_${q}_${delta}_${mu}_${omega}
-		title="q=$q;\delta=$delta;omega=$omega;mu=$mu"
-    ~/base/pypismtools/scripts/basemap-plot.py -v cbase --inner_titles $title --colorbar_label -p twocol --singlerow --colormap ~/base/pypismtools/colormaps/Full_saturation_spectrum_CCW_orange.cpt -o ${filepre}_cbase.pdf ${filepre}.nc
+		title="q=$q;"'$\delta$'"=$delta;"'$\omega$'"=$omega;"'$\mu$'"=$mu"
+		for var in  "cbase" "csurf"; do
+		    ~/base/pypismtools/scripts/basemap-plot.py -v $var --inner_titles $title --colorbar_label -p twocol --singlerow --colormap ~/base/pypismtools/colormaps/Full_saturation_spectrum_CCW_orange.cpt  -o ${filepre}_${var}.pdf ${filepre}.nc
+	done
 	    done
 	done
 	filepre=g${GRID}km_${q}_${delta}_hydro_null
-	title="q=$q;\delta=$delta;ctrl"
-    ~/base/pypismtools/scripts/basemap-plot.py -v cbase --inner_titles $title --colorbar_label -p height --singlerow --colormap ~/base/pypismtools/colormaps/Full_saturation_spectrum_CCW_orange.cpt  -o ${filepre}_cbase.pdf ${filepre}.nc
+	title="q=$q;"'$\delta$'"=$delta;"'$\omega$'"=$omega;"'$\mu$'"=$mu"
+	for var in "cbase" "csurf"; do
+    ~/base/pypismtools/scripts/basemap-plot.py -v $var --inner_titles $title --colorbar_label -p twocol --singlerow --colormap ~/base/pypismtools/colormaps/Full_saturation_spectrum_CCW_orange.cpt  -o ${filepre}_${var}.pdf ${filepre}.nc
+	done
     done
 done
 
