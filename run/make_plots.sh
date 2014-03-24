e9af4da6c83b58d7b24e76c8c589d683a8e642eb
Andy Aschwanden <andy.aschwanden@gmail.com>
2014-03-23 18:42:06 -0800

    working on plot script

---
+++ run/make_plots.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x -e
+
+GRID=10
+
+for q in 0.1 0.25; do
+    for delta in 0.01 0.02 0.05; do
+	for mu in 1e-5 5e-5 1e-6 5e-6; do
+	    for omega in 1 10 100 1000 10000; do
+		filepre=g${GRID}km_${q}_${delta}_${mu}_${omega}
+		title="q=$q;\delta=$delta;omega=$omega;mu=$mu"
+    ~/base/pypismtools/scripts/basemap-plot.py -v cbase --inner_titles $title --colorbar_label -p twocol --singlerow --colormap ~/base/pypismtools/colormaps/Full_saturation_spectrum_CCW_orange.cpt -o ${filepre}_cbase.pdf ${filepre}.nc
+	    done
+	done
+	filepre=g${GRID}km_${q}_${delta}_hydro_null
+	title="q=$q;\delta=$delta;ctrl"
+    ~/base/pypismtools/scripts/basemap-plot.py -v cbase --inner_titles $title --colorbar_label -p height --singlerow --colormap ~/base/pypismtools/colormaps/Full_saturation_spectrum_CCW_orange.cpt  -o ${filepre}_cbase.pdf ${filepre}.nc
+    done
+done
+
+#--geotiff_file MODISGreenland1kmclean_cut.tif
