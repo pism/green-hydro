d9a758e70071f534a84a55bfb0e3d4b153d5f0de
Andy Aschwanden <andy.aschwanden@gmail.com>
2014-03-25 17:52:28 -0800

    - grid sequencing for spinup should work now.

--- run/make_plots.sh
+++ run/make_plots.sh
@@ -4,7 +4,7 @@ set -x -e
 
 GRID=10
 
-for q in 0.1 0.25; do
+for q in 0.1 0.25 0.8; do
     for delta in 0.01 0.02 0.05; do
 	for mu in 1e-5 5e-5 1e-6 5e-6; do
 	    for omega in 1 10 100 1000 10000; do
@@ -20,7 +20,7 @@ for q in 0.1 0.25; do
 	title="q=$q;"'$\delta$'"=$delta;ctrl"
 	for var in "cbase" "csurf"; do
 	    echo "plotting $var from ${filepre}.nc"
-	    ~/base/pypismtools/scripts/basemap-plot.py -v $var --inner_titles $title --colorbar_label -p twocol --singlerow --colormap ~/base/pypismtools/colormaps/Full_saturation_spectrum_CCW_orange.cpt --geotiff_file MODISGreenland1kmclean_cut.tif -o ${filepre}_${var}.pdf ${filepre}.nc
+	    ~/base/pypismtools/scripts/basemap-plot.py -v $var --inner_titles $title --colorbar_label -p twocol --singlerow --colormap ~/base/pypismtools/colormaps/Full_saturation_spectrum_CCW_orange.cpt --geotiff_file MODISGreenland1kmclean_cut.tif  -o ${filepre}_${var}.pdf ${filepre}.nc
 	done
     done
 done
---
+++ run/spinup-hotspot-gridseq.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+
+# Copyright (C) 2014 Andy Aschwanden and Ed Bueler
+
+set -e  # exit on error
+
+#  creates spinup scripts using grid sequencing 20->10->5km.
+#  scripts are suitable for PBS job scheduler
+#  (see  http://www.adaptivecomputing.com/products/open-source/torque/)
+#
+#     $ ./spinup-hotspot-gridseq.sh paleo     ## do paleo-climate spinup
+#     or
+#     $ ./spinup-hotspot-gridseq.sh const     ## do paleo-climate spinup
+#  then, assuming you like the resulting scripts:
+#     $ qsub do-climate-spinup-hotspot.sh      ### <--- REALLY SUBMITS using qsub
+
+
+set -e # exit on error
+CLIMLIST="{const, paleo}"
+# set coupler from argument 2
+if [ "$1" = "const" ]; then
+    CLIMATE=$1
+elif [ "$1" = "paleo" ]; then
+    CLIMATE=$1
+else
+  echo "invalid second argument; must be in $CLIMLIST"
+  exit
+fi
+
+SCRIPTNAME=${CLIMATE}-spinup-hotspot-gridref.sh
+export PISM_EXPERIMENT=$EXPERIMENT
+export PISM_TITLE="Greenland Parameter Study"
+
+GRID=20
+INFILE=pism_Greenland_${GRID}km_v2_hotspot.nc
+PISM_DATANAME=$INFILE
+DURA=100000
+START=-125000
+END=-25000
+
+WALLTIME=12:00:00
+NODES=2
+NN=8
+
+ SHEBANGLINE="#!/bin/bash"
+MPIQUEUELINE="#PBS -q standard_4"
+ MPITIMELINE="#PBS -l walltime=$WALLTIME"
+ MPISIZELINE="#PBS -l nodes=$NODES:ppn=4"
+  MPIOUTLINE="#PBS -j oe"
+
+SCRIPT="do_${GRID}km_${CLIMATE}-spinup-hotspot.sh"
+rm -f $SCRIPT
+EXPERIMENT="${DURAKA}ka ${CLIMATE}-climate initialization with hotspot"
+MKA=$(($END/-1000))
+
+OUTFILE=g${GRID}km_m${MKA}ka_${CLIMATE}_hotspot.nc
+
+# insert preamble
+echo $SHEBANGLINE >> $SCRIPT
+echo >> $SCRIPT # add newline
+echo $MPIQUEUELINE >> $SCRIPT
+echo $MPITIMELINE >> $SCRIPT
+echo $MPISIZELINE >> $SCRIPT
+echo $MPIOUTLINE >> $SCRIPT
+echo >> $SCRIPT # add newline
+echo "cd \$PBS_O_WORKDIR" >> $SCRIPT
+echo >> $SCRIPT # add newline
+      
+cmd="PISM_DO="" STARTEND=$START,$END PISM_DATANAME=$PISM_DATANAME  ./run.sh $NN $CLIMATE $DURA $GRID hybrid null $OUTFILE $INFILE"
+echo "$cmd 2>&1 | tee job.\${PBS_JOBID}" >> $SCRIPT
+
+echo "($SPAWNSCRIPT)  $SCRIPT written"
+
+GRID=10
+SCRIPTNAME=${GRID}km_${CLIMATE}-spinup-hotspot-gridref.sh
+INFILE=pism_Greenland_${GRID}km_v2_hotspot.nc
+REGRIDFILE=$OUTFILE
+PISM_DATANAME=$INFILE
+DURA=20000
+START=-25000
+END=-5000
+
+WALLTIME=24:00:00
+NODES=4
+NN=32
+
+ SHEBANGLINE="#!/bin/bash"
+MPIQUEUELINE="#PBS -q standard_4"
+ MPITIMELINE="#PBS -l walltime=$WALLTIME"
+ MPISIZELINE="#PBS -l nodes=$NODES:ppn=4"
+  MPIOUTLINE="#PBS -j oe"
+
+SCRIPT="do_${GRID}km_${CLIMATE}-spinup-hotspot.sh"
+rm -f $SCRIPT
+EXPERIMENT="${DURAKA}ka ${CLIMATE}-climate initialization with hotspot"
+MKA=$(($END/-1000))
+
+OUTFILE=g${GRID}km_m${MKA}ka_${CLIMATE}_hotspot.nc
+
+# insert preamble
+echo $SHEBANGLINE >> $SCRIPT
+echo >> $SCRIPT # add newline
+echo $MPIQUEUELINE >> $SCRIPT
+echo $MPITIMELINE >> $SCRIPT
+echo $MPISIZELINE >> $SCRIPT
+echo $MPIOUTLINE >> $SCRIPT
+echo >> $SCRIPT # add newline
+echo "cd \$PBS_O_WORKDIR" >> $SCRIPT
+echo >> $SCRIPT # add newline
+      
+echo >> $SCRIPT
+cmd="PISM_DO="" STARTEND=$START,$END PISM_DATANAME=$PISM_DATANAME REGRIDFILE=$REGRIDFILE ./run.sh $NN $CLIMATE $DURA $GRID hybrid null $OUTFILE $INFILE"
+echo "$cmd 2>&1 | tee job.\${PBS_JOBID}" >> $SCRIPT
+echo "($SPAWNSCRIPT)  $SCRIPT written"
+	      
+GRID=5
+SCRIPTNAME=${GRID}km_${CLIMATE}-spinup-hotspot-gridref.sh
+INFILE=pism_Greenland_${GRID}km_v2_hotspot.nc
+REGRIDFILE=$OUTFILE
+PISM_DATANAME=$INFILE
+DURA=5000
+START=-5000
+END=0
+
+WALLTIME=96:00:00
+NODES=8
+NN=64
+
+ SHEBANGLINE="#!/bin/bash"
+MPIQUEUELINE="#PBS -q standard_4"
+ MPITIMELINE="#PBS -l walltime=$WALLTIME"
+ MPISIZELINE="#PBS -l nodes=$NODES:ppn=4"
+  MPIOUTLINE="#PBS -j oe"
+
+SCRIPT="do_${GRID}km_${CLIMATE}-spinup-hotspot.sh"
+rm -f $SCRIPT
+EXPERIMENT="${DURAKA}ka ${CLIMATE}-climate initialization with hotspot"
+MKA=$(($END/-1000))
+
+OUTFILE=g${GRID}km_m${MKA}ka_${CLIMATE}_hotspot.nc
+
+# insert preamble
+echo $SHEBANGLINE >> $SCRIPT
+echo >> $SCRIPT # add newline
+echo $MPIQUEUELINE >> $SCRIPT
+echo $MPITIMELINE >> $SCRIPT
+echo $MPISIZELINE >> $SCRIPT
+echo $MPIOUTLINE >> $SCRIPT
+echo >> $SCRIPT # add newline
+echo "cd \$PBS_O_WORKDIR" >> $SCRIPT
+echo >> $SCRIPT # add newline
+      
+echo >> $SCRIPT
+cmd="PISM_DO="" STARTEND=$START,$END PISM_DATANAME=$PISM_DATANAME REGRIDFILE=$REGRIDFILE PARAM_FTT="foo" ./run.sh $NN $CLIMATE $DURA $GRID hybrid null $OUTFILE $INFILE"
+echo "$cmd 2>&1 | tee job.\${PBS_JOBID}" >> $SCRIPT
+echo "($SPAWNSCRIPT)  $SCRIPT written"
