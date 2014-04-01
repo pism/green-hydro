74cec50ebc5759ffe7feed49609a1f715c611b4a
Andy Aschwanden <andy.aschwanden@gmail.com>
2014-04-01 13:08:28 -0800

    - added missing hotspot script

--- run/spinup.sh
+++ run/spinup.sh
@@ -8,16 +8,35 @@ set -e  # exit on error
 #  scripts are suitable for PBS job scheduler
 #  (see  http://www.adaptivecomputing.com/products/open-source/torque/)
 #
-#     $ ./spinup-hotspot-gridseq.sh paleo     ## do paleo-climate spinup
+#     $ ./spinup.sh paleo 970mW_hs    ## do paleo-climate spinup with 970mW hotspot
 #     or
-#     $ ./spinup-hotspot-gridseq.sh const     ## do paleo-climate spinup
+#     $ ./spinup.sh const     ## do constant -climate spinup
 #  then, assuming you like the resulting scripts:
-#     $ qsub do-climate-spinup-hotspot.sh      ### <--- REALLY SUBMITS using qsub
+#     $ qsub do-climate-spinup.sh      ### <--- REALLY SUBMITS using qsub
 
 
 set -e # exit on error
+
 CLIMLIST="{const, paleo}"
-# set coupler from argument 2
+TYPELIST="{ctrl, 970mW_hs}"
+if [ $# -lt 2 ] ; then
+  echo "spinup.sh ERROR: needs 2 positional arguments ... ENDING NOW"
+  echo
+  echo "usage:"
+  echo
+  echo "    spinup.sh CLIMATE TYPE"
+  echo
+  echo "  where:"
+  echo "    CLIMATE   in $CLIMLIST"
+  echo "    TYPE      in $TYPELIST"
+  echo
+  echo
+  exit
+fi
+
+
+
+# set coupler from argument 1
 if [ "$1" = "const" ]; then
     CLIMATE=$1
 elif [ "$1" = "paleo" ]; then
@@ -27,13 +46,22 @@ else
   exit
 fi
 
-SCRIPTNAME=${CLIMATE}-spinup-hotspot-gridref.sh
+# set TYPE from argument 1
+if [ "$2" = "ctrl" ]; then
+    TYPE=$2
+elif [ "$2" = "970mW_hs" ]; then
+    TYPE=$2
+else
+  echo "invalid second argument; must be in $TYPELIST"
+  exit
+fi
+
+SCRIPTNAME=${CLIMATE}-spinup-${TYPE}.sh
 export PISM_EXPERIMENT=$EXPERIMENT
 export PISM_TITLE="Greenland Parameter Study"
 
 GRID=20
-INFILE=pism_Greenland_${GRID}km_v2_hotspot.nc
-#INFILE=g20km_steady_ssa.nc
+INFILE=pism_Greenland_${GRID}km_v2_${TYPE}.nc
 PISM_DATANAME=$INFILE
 DURA=100000
 START=-125000
@@ -49,12 +77,12 @@ MPIQUEUELINE="#PBS -q standard_4"
  MPISIZELINE="#PBS -l nodes=$NODES:ppn=4"
   MPIOUTLINE="#PBS -j oe"
 
-SCRIPT="do_${GRID}km_${CLIMATE}-spinup-hotspot.sh"
+SCRIPT="do_${GRID}km_${CLIMATE}-spinup-${TYPE}.sh"
 rm -f $SCRIPT
-EXPERIMENT="${DURAKA}ka ${CLIMATE}-climate initialization with hotspot"
+EXPERIMENT="${DURAKA}ka ${CLIMATE}-climate initialization $TYPE"
 MKA=$(($END/-1000))
 
-OUTFILE=g${GRID}km_m${MKA}ka_${CLIMATE}_hotspot.nc
+OUTFILE=g${GRID}km_m${MKA}ka_${CLIMATE}_${TYPE}.nc
 
 # insert preamble
 echo $SHEBANGLINE >> $SCRIPT
@@ -73,8 +101,8 @@ echo "$cmd 2>&1 | tee job.\${PBS_JOBID}" >> $SCRIPT
 echo "($SPAWNSCRIPT)  $SCRIPT written"
 
 GRID=10
-SCRIPTNAME=${GRID}km_${CLIMATE}-spinup-hotspot-gridref.sh
-INFILE=pism_Greenland_${GRID}km_v2_hotspot.nc
+SCRIPTNAME=${GRID}km_${CLIMATE}-spinup-${TYPE}.sh
+INFILE=pism_Greenland_${GRID}km_v2_${TYPE}.nc
 REGRIDFILE=$OUTFILE
 PISM_DATANAME=$INFILE
 DURA=20000
@@ -91,12 +119,12 @@ MPIQUEUELINE="#PBS -q standard_4"
  MPISIZELINE="#PBS -l nodes=$NODES:ppn=4"
   MPIOUTLINE="#PBS -j oe"
 
-SCRIPT="do_${GRID}km_${CLIMATE}-spinup-hotspot.sh"
+SCRIPT="do_${GRID}km_${CLIMATE}-spinup-${TYPE}.sh"
 rm -f $SCRIPT
-EXPERIMENT="${DURAKA}ka ${CLIMATE}-climate initialization with hotspot"
+EXPERIMENT="${DURAKA}ka ${CLIMATE}-climate initialization $TYPE"
 MKA=$(($END/-1000))
 
-OUTFILE=g${GRID}km_m${MKA}ka_${CLIMATE}_hotspot.nc
+OUTFILE=g${GRID}km_m${MKA}ka_${CLIMATE}_${TYPE}.nc
 
 # insert preamble
 echo $SHEBANGLINE >> $SCRIPT
@@ -115,8 +143,8 @@ echo "$cmd 2>&1 | tee job.\${PBS_JOBID}" >> $SCRIPT
 echo "($SPAWNSCRIPT)  $SCRIPT written"
 	      
 GRID=5
-SCRIPTNAME=${GRID}km_${CLIMATE}-spinup-hotspot-gridref.sh
-INFILE=pism_Greenland_${GRID}km_v2_hotspot.nc
+SCRIPTNAME=${GRID}km_${CLIMATE}-spinup-${TYPE}.sh
+INFILE=pism_Greenland_${GRID}km_v2_${TYPE}.nc
 REGRIDFILE=$OUTFILE
 PISM_DATANAME=$INFILE
 DURA=5000
@@ -133,12 +161,12 @@ MPIQUEUELINE="#PBS -q standard_4"
  MPISIZELINE="#PBS -l nodes=$NODES:ppn=4"
   MPIOUTLINE="#PBS -j oe"
 
-SCRIPT="do_${GRID}km_${CLIMATE}-spinup-hotspot.sh"
+SCRIPT="do_${GRID}km_${CLIMATE}-spinup-${TYPE}.sh"
 rm -f $SCRIPT
 EXPERIMENT="${DURAKA}ka ${CLIMATE}-climate initialization with hotspot"
 MKA=$(($END/-1000))
 
-OUTFILE=g${GRID}km_0_${CLIMATE}_hotspot.nc
+OUTFILE=g${GRID}km_0_${CLIMATE}_${TYPE}.nc
 
 # insert preamble
 echo $SHEBANGLINE >> $SCRIPT
