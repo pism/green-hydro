8790a7ac0c105fefe32fca0d84442cdadf4724b9
Andy Aschwanden <andy.aschwanden@gmail.com>
2014-05-03 16:19:15 -0800

    cleaned-up and improved postprocessing. Should now work on pacman

--- run/run-postpro.sh
+++ run/run-postpro.sh
@@ -69,28 +69,26 @@ if [ -f ${filepre}.nc ]; then
 
     ogr2ogr -overwrite -t_srs EPSG:4326 ${spc_dir}/${filepre}_speed_contours_epsg4326.shp ${spc_dir}/${filepre}_speed_contours.shp
 
-    basemap-plot.py -v velsurf_mag --inner_titles $title --colorbar_label -p medium --singlerow --shape_file surf_vels_mag_contours_epsg4326.shp ${spc_dir}/${filepre}_speed_contours_epsg4326.shp --colormap ~/base/pypismtools/colormaps/Full_saturation_spectrum_CCW_orange.cpt -r $res --map_resolution $mres --geotiff_file MODIS$Jakobshavn1km.tif -o ${fig_dir}/Jakobshavn_${filepre}_velsurf_mag.pdf ${nc_dir}/${filepre}.nc
+    basemap-plot.py -v velsurf_mag --inner_titles "$title" --colorbar_label -p medium --singlerow --shape_file surf_vels_mag_contours_epsg4326.shp ${spc_dir}/${filepre}_speed_contours_epsg4326.shp --colormap Full_saturation_spectrum_CCW_orange.cpt -r $res --map_resolution $mres --geotiff_file MODIS$Jakobshavn1km.tif -o ${fig_dir}/Jakobshavn_${filepre}_velsurf_mag.pdf ${nc_dir}/${filepre}.nc
 
-    basemap-plot.py -v velsurf_mag --inner_titles $title --colorbar_label -p medium --singlerow --shape_file surf_vels_mag_contours_epsg4326.shp ${spc_dir}/${filepre}_speed_contours_epsg4326.shp --colormap ~/base/pypismtools/colormaps/Full_saturation_spectrum_CCW_orange.cpt -r $res --map_resolution $mres --geotiff_file MODISKangerdlugssuaq1km.tif -o ${fig_dir}/Kangerdlugssuaq_${filepre}_velsurf_mag.pdf ${nc_dir}/${filepre}.nc
+    basemap-plot.py -v velsurf_mag --inner_titles "$title" --colorbar_label -p medium --singlerow --shape_file surf_vels_mag_contours_epsg4326.shp ${spc_dir}/${filepre}_speed_contours_epsg4326.shp --colormap Full_saturation_spectrum_CCW_orange.cpt -r $res --map_resolution $mres --geotiff_file MODISKangerdlugssuaq1km.tif -o ${fig_dir}/Kangerdlugssuaq_${filepre}_velsurf_mag.pdf ${nc_dir}/${filepre}.nc
 
-    basemap-plot.py -v velsurf_mag --inner_titles velsurf_mag --colorbar_label -p medium --singlerow --colormap ~/base/pypismtools/colormaps/Full_saturation_spectrum_CCW_orange.cpt -r $res --coastlines $geotiff -o ${fig_dir}/Greenland_${filepre}_velsurf_mag.pdf ${nc_dir}/${filepre}.nc
+    basemap-plot.py -v velsurf_mag --inner_titles velsurf_mag --colorbar_label -p medium --singlerow --colormap Full_saturation_spectrum_CCW_orange.cpt -r $res --coastlines $geotiff -o ${fig_dir}/Greenland_${filepre}_velsurf_mag.pdf ${nc_dir}/${filepre}.nc
 
-    basemap-plot.py -v velbase_mag --inner_titles velsurf_mag --colorbar_label -p medium --singlerow --colormap ~/base/pypismtools/colormaps/Full_saturation_spectrum_CCW_orange.cpt -r $res --coastlines $geotiff -o ${fig_dir}/Greenland_${filepre}_velbase_mag.pdf ${nc_dir}/${filepre}.nc
+    basemap-plot.py -v velbase_mag --inner_titles velsurf_mag --colorbar_label -p medium --singlerow --colormap Full_saturation_spectrum_CCW_orange.cpt -r $res --coastlines $geotiff -o ${fig_dir}/Greenland_${filepre}_velbase_mag.pdf ${nc_dir}/${filepre}.nc
 
     basemap-plot.py -v tau_r --inner_titles tau_r --colorbar_label -p medium --singlerow -r $res --coastlines $geotiff -o ${fig_dir}/Greenland_${filepre}_tau_r.pdf ${nc_dir}/${filepre}.nc
-
-    basemap-plot.py -v tau_rel --inner_titles $var -p medium --singlerow -r $res --map_resolution --coastlines $geotiff -o ${fig_dir}/Greenland_${filepre}_tau_rel.pdf ${nc_dir}/${filepre}.nc
 
     # create latex file
     rm -f Greenland_${filepre}.tex
     cat - > Greenland_${filepre}.tex <<EOLF
-\documentclass[a4paper,landscape]{article}
+\documentclass[a4paper]{article}
 \usepackage[margin=2mm,nohead,nofoot]{geometry}
 \usepackage{pdfpages}
 \usepackage[multidot]{grffile}
 \parindent0pt
 \\begin{document}
-\includepdfmerge[nup=2x2,landscape,pagecommand={\thispagestyle{myheadings}\markright{\huge{$title}}}]{${fig_dir}/Greenland_${filepre}_velsurf_mag.pdf,1,${fig_dir}/Greenland_${filepre}_velbase_mag.pdf,1,${fig_dir}/Greenland_${filepre}_tau_rel.pdf,1,${fig_dir}/Greenland_${filepre}_tau_r.pdf,1}
+\includepdfmerge[nup=1x3,pagecommand={\thispagestyle{myheadings}\markright{\huge{$title}}}]{${fig_dir}/Greenland_${filepre}_velsurf_mag.pdf,1,${fig_dir}/Greenland_${filepre}_velbase_mag.pdf,1,${fig_dir}/Greenland_${filepre}_tau_r.pdf,1}
 \end{document}
 EOLF
     pdflatex Greenland_${filepre}
