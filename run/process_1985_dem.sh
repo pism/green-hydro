bf80d0baadb8191c6305cd8655f604d8880cee30
Andy Aschwanden <andy.aschwanden@gmail.com>
2014-05-12 20:15:33 -0800

    a little bit DEM-ing.

--- run/process_1985_dem.sh
+++ run/process_1985_dem.sh
@@ -22,19 +22,43 @@ GIMP=gimpdem_90m
 wget -nc ftp://ftp-bprc.mps.ohio-state.edu/downloads/gdg/gimpdem/${GIMP}.tif
 
 # Remap 1985 to GIMP DEM projection, dimensions, and resolution
-gdalwarp -overwrite -srcnodata 1.69971e+38 -dstnodata -2e9 -s_srs epsg:32622 -t_srs epsg:3413 -tr 90 90 -te -639955 -3355595 855845 -655595 -r average -cutline $CUT -of GTiff ${DATANAME}.dat ${DATANAME}_epsg3413.tif
+gdalwarp -overwrite -srcnodata 1.69971e+38 -dstnodata 0 -s_srs EPSG:32622 -t_srs EPSG:3413 -tr 90 90 -te -639955 -3355595 855845 -655595 -r bilinear -cutline $CUT -of netCDF  ${DATANAME}.dat ${DATANAME}_epsg3413.nc
+ncatted -a _FillValue,Band1,d,, ${DATANAME}_epsg3413.nc
 
+gdal_translate -of netCDF ${GIMP}.tif ${GIMP}.nc
+
+cdo add  ${GIMP}.nc ${DATANAME}_epsg3413.nc usurf_90m_1985.nc
+ncks -A -v x,y,polar_stereographic ${GIMP}.nc usurf_90m_1985.nc
+ncatted -a grid_mapping,Band1,o,c,"polar_stereographic" usurf_90m_1985.nc
 #
-OUT=usurf_90m_1985_epsg3413
-gdal_merge.py -n -2e9 -of GTiff -o $OUT.tif ${GIMP}.tif ${DATANAME}_epsg3413.tif
-
-IN=$OUT
 OUT=usurf_1km_1985
 
-gdalwarp -overwrite -s_srs epsg:3413 -t_srs "+proj=stere +lat_0=90 +lat_ts=71 +lon_0=-39 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs" -te -800000.0 -3400000.0 700000.0 -600000.0 -tr 1000 1000 -r average -of GTiff $IN.tif  $OUT.tif
+# These are the cell-centers of the corner grids
+# llx=-800000.0 
+# lly=-3400000.0 
+# urx=700000.0 
+# ury=-600000.0
+# enlarge domain by 1 cell to get cell-center vs corner right
+llx=-800500.0 
+lly=-3400500. 
+urx=700500.0 
+ury=-599500.0
+TE="$llx $lly $urx $ury"
+gdalwarp -overwrite -s_srs epsg:3413 -t_srs "+proj=stere +lat_0=90 +lat_ts=71 +lon_0=-39 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs" -te $TE -tr 1000 1000 -r average -of GTiff usurf_90m_1985.nc  $OUT.tif
 
 gdal_translate -of netCDF $OUT.tif $OUT.nc
 
+
+ncrename -v Band1,usurf $OUT.nc
+
+
+cdo sub -selvar,usurf $OUT.nc -selvar,usurf pism_Greenland_1km_v3.nc geoid_correction.nc
+THK=thk_1985
+cdo sub $OUT.nc geoid_correction.nc ${OUT}_corrected.nc
+ncks -O  -v usurf -x pism_Greenland_1km_v3.nc $THK.nc
+ncks -A -v usurf  ${OUT}_corrected.nc $THK.nc
+ncap2 -O -s "thk=usurf-topg; where(LandMask!=2) thk=0;" $THK.nc $THK.nc
+ncks -A -v x,y pism_Greenland_1km_v3.nc $THK.nc
 exit
 ncatted -a _FillValue,Band1,o,f,-2e9 $OUT.nc
 fill_missing.py -v Band1 -f $OUT.nc -o usurf_1985.nc
