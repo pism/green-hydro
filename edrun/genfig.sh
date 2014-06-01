d9a319ed1b2e3af1a56344ba259c76bca19fd6f7
Ed Bueler <elbueler@alaska.edu>
2014-06-01 01:07:00 -0800

    Brought over showPvsW.py from pism/examples/nbreen/.  Generated first draft figures for paper; note .png is actually better here.

--- edrun/genfig.sh
+++ edrun/genfig.sh
@@ -11,7 +11,7 @@
 FILENAME=$1
 OUTROOT=$2
 
-./showPvsW.py -wmin 0.0 -wmax 0.15 -c thk -cmin 0 -cmax 2000 -s hydrovelbase_mag -smin 500  -smax 5000 -o bin500-${OUTROOT} $FILENAME
+./showPvsW.py -wmin 0.0 -wmax 0.15 -c thk -cmin 0 -cmax 2000 -s hydrovelbase_mag -smin 500  -smax 5000 -o bin500-${OUTROOT} --colorbar $FILENAME
 ./showPvsW.py -wmin 0.0 -wmax 0.15 -c thk -cmin 0 -cmax 2000 -s hydrovelbase_mag -smin 100  -smax 500  -o bin100-${OUTROOT}  $FILENAME
 ./showPvsW.py -wmin 0.0 -wmax 0.15 -c thk -cmin 0 -cmax 2000 -s hydrovelbase_mag -smin 30   -smax 100  -o bin30-${OUTROOT}   $FILENAME
 ./showPvsW.py -wmin 0.0 -wmax 0.15 -c thk -cmin 0 -cmax 2000 -s hydrovelbase_mag -smin 10   -smax 30   -o bin10-${OUTROOT}   $FILENAME
