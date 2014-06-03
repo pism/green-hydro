fce655c861e1480945ebb689879cc93db0f68cdc
Andy Aschwanden <andy.aschwanden@gmail.com>
2014-06-02 16:47:45 -0800

    funky stuff with PAT

--- run/run-postpro.sh
+++ run/run-postpro.sh
@@ -52,7 +52,7 @@ if [ -f ${filepre}.nc ]; then
     ncpdq -O -a time,y,x,z,zb ${filepre}.nc ${tl_dir}/${nc_dir}/${filepre}.nc
     ncap2 -O -s "where(thk<1) {velbase_mag=$fill; velsurf_mag=$fill; flux_mag=$fill;}; tau_r = tauc/(taud_mag+1); tau_rel=(tauc-taud_mag)/(1+taud_mag)" ${tl_dir}/${nc_dir}/${filepre}.nc ${tl_dir}/${nc_dir}/${filepre}.nc
     ncatted -a units,tau_rel,o,c,"1" ${tl_dir}/${nc_dir}/${filepre}.nc
-    python extract-profiles.py $fluxgates ${filepre}.nc ${tl_dir}/${pr_dir}/profiles_${filepre}.nc
+    extract-profiles.py $fluxgates ${filepre}.nc ${tl_dir}/${pr_dir}/profiles_${filepre}.nc
 fi
 
 EOF
