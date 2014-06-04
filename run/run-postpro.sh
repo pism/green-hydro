570b597ff0ef7de64937d7b853b9e0179a02cdb7
Andy Aschwanden <andy.aschwanden@gmail.com>
2014-06-04 13:19:30 -0800

    added velshear_mag calculation and plot

--- run/run-postpro.sh
+++ run/run-postpro.sh
@@ -50,7 +50,7 @@ if [ -f ${filepre}.nc ]; then
     # because QGIS doesn't like (x,y) ordering
     sh add_epsg3413_mapping.sh ${filepre}.nc
     ncpdq -O -v enthalpy,litho_temp,temp_pa,liqfrac -x -a time,y,x ${filepre}.nc ${tl_dir}/${nc_dir}/${filepre}.nc
-    ncap2 -O -s "where(thk<1) {velbase_mag=$fill; velsurf_mag=$fill; flux_mag=$fill;}; tau_r = tauc/(taud_mag+1); tau_rel=(tauc-taud_mag)/(1+taud_mag)" ${tl_dir}/${nc_dir}/${filepre}.nc ${tl_dir}/${nc_dir}/${filepre}.nc
+    ncap2 -O -s "velshear_mag=velsurf_mag-velbase_mag; where(thk<1) {velshear_mag=$fill; velbase_mag=$fill; velsurf_mag=$fill; flux_mag=$fill;}; tau_r = tauc/(taud_mag+1); tau_rel=(tauc-taud_mag)/(1+taud_mag)" ${tl_dir}/${nc_dir}/${filepre}.nc ${tl_dir}/${nc_dir}/${filepre}.nc
     ncatted -a units,tau_rel,o,c,"1" ${tl_dir}/${nc_dir}/${filepre}.nc
     extract-profiles.py $fluxgates ${filepre}.nc ${tl_dir}/${pr_dir}/profiles_${filepre}.nc
 fi
@@ -108,6 +108,8 @@ if [ -f ${tl_dir}/${nc_dir}/${filepre}.nc ]; then
 
     basemap-plot.py -v velbase_mag --inner_titles velsurf_mag --colorbar_label -p medium --singlerow --colormap Full_saturation_spectrum_CCW_orange.cpt -r $res  $geotiff -o ${tl_dir}/${fig_dir}/Greenland_${filepre}_velbase_mag.pdf ${tl_dir}/${nc_dir}/${filepre}.nc
 
+    basemap-plot.py -v velbase_mag --inner_titles velshear_mag --colorbar_label -p medium --singlerow --colormap Full_saturation_spectrum_CCW_orange.cpt -r $res  $geotiff -o ${tl_dir}/${fig_dir}/Greenland_${filepre}_velshear_mag.pdf ${tl_dir}/${nc_dir}/${filepre}.nc
+
     basemap-plot.py -v tau_r --inner_titles tau_r --colorbar_label -p medium --singlerow -r $res  $geotiff -o ${tl_dir}/${fig_dir}/Greenland_${filepre}_tau_r.pdf ${tl_dir}/${nc_dir}/${filepre}.nc
 
     # create latex file
@@ -119,7 +121,7 @@ if [ -f ${tl_dir}/${nc_dir}/${filepre}.nc ]; then
 \usepackage[multidot]{grffile}
 \parindent0pt
 \\begin{document}
-\includepdfmerge[nup=1x3,landscape,pagecommand={\thispagestyle{myheadings}\markright{\huge{$title}}}]{${tl_dir}/${fig_dir}/Greenland_${filepre}_velsurf_mag.pdf,1,${tl_dir}/${fig_dir}/Greenland_${filepre}_velbase_mag.pdf,1,${tl_dir}/${fig_dir}/Greenland_${filepre}_tau_r.pdf,1}
+\includepdfmerge[nup=2x2,landscape,pagecommand={\thispagestyle{myheadings}\markright{\huge{$title}}}]{${tl_dir}/${fig_dir}/Greenland_${filepre}_velsurf_mag.pdf,1,${tl_dir}/${fig_dir}/Greenland_${filepre}_velbase_mag.pdf,${tl_dir}/${fig_dir}/Greenland_${filepre}_velshear_mag.pdf,1,${tl_dir}/${fig_dir}/Greenland_${filepre}_tau_r.pdf,1}
 \end{document}
 EOLF
     pdflatex Greenland_${filepre}
