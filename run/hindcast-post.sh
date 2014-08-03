6921e13519c7fb2e2d5b958d2064d9d57ca8301c
Andy Aschwanden <aaschwanden@alaska.edu>
2014-08-02 16:14:31 -0800

    trying to simply post processing. might be too memory intensive

--- run/hindcast-post.sh
+++ run/hindcast-post.sh
@@ -13,11 +13,10 @@ start=2000
 while [ $start -lt 2007 ]; do
     echo $start
     end=$[$start+1]
-    for var in "bmelt" "bwat" "sigma_xx" "sigma_yy" "sigma_xy" "diffusivity" "h_x_i" "h_x_j" "h_y_i" "h_y_j" "mask" "eigen1" "eigen2" "taub_x" "taub_y" "taud_x" "taud_y" "taud_mag" "tempicethk_basal" "temppabase" "thk" "topg" "usurf" "uvelbase" "vvelbase" "uvelsurf" "vvelsurf" "velsurf_mag" "velbase_mag" "wvelbase" "wvelsurf" "tillwat"; do
-        echo $var
-        POST=post_${start}-${end}_${var}.sh
+    
+    POST=post_${start}-${end}.sh
 
-        cat - > $POST <<EOF
+    cat - > $POST <<EOF
 
 $MYSHEBANGLINE
 $MYMPIQUEUELINE
@@ -36,14 +35,13 @@ if [ ! -d ${reg_dir} ]; then
 fi
 
   sh add_epsg3413_mapping.sh ex_g1500m_hydro_null_${start}-${end}.nc
-  ncpdq -O -4 -L 3 -a time,y,x -v ${var} ex_g1500m_hydro_null_${start}-${end}.nc ${out_dir}/ex_g1500m_hydro_null_${start}-${end}_${var}.nc
-  ncks -A -v pism_overrides,pism_config ex_g1500m_hydro_null_${start}-${end}.nc ${out_dir}/ex_g1500m_hydro_null_${start}-${end}_${var}.nc
-  nc2cdo.py --srs "+init=epsg:3413" ${out_dir}/ex_g1500m_hydro_null_${start}-${end}_${var}.nc
-  ncks -d x,-222000.,-60000. -d y,-2330000.,-2230000. ${out_dir}/ex_g1500m_hydro_null_${start}-${end}_${var}.nc ${reg_dir}/Jakobshavn_ex_g1500m_hydro_null_${start}-${end}_${var}.nc
+  ncpdq -O -4 -L 3 -a time,y,x -d x,-222000.,-60000. -d y,-2330000.,-2230000. ex_g1500m_hydro_null_${start}-${end}.nc ${reg_dir}/Jakobshavn_ex_g1500m_hydro_null_${start}-${end}.nc
+  nc2cdo.py --srs "+init=epsg:3413" ${reg_dir}/Jakobshavn_ex_g1500m_hydro_null_${start}-${end}.nc
+
 EOF
 
-        qsub $POST
-    done			  
+    qsub $POST
+			  
     start=$[$start+1]
 done
 
