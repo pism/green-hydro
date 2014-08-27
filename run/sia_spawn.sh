28a1c756109eb20bdffba7e5b7473738fc73a814
Andy Aschwanden <aaschwanden@alaska.edu>
2014-08-26 18:00:12 -0800

    get going on smarter pruning, start with SIA-only in slow flowing areas.

--- run/sia_spawn.sh
+++ run/sia_spawn.sh
@@ -137,7 +137,7 @@ MPIQUEUELINE="#PBS -q $QUEUE"
 # set up parameter sensitivity study: Shallow Ice Approximation
 # ########################################################
 
-for E in 1 1.25 1.5 2 3; do
+for E in 1 1.25 1.5 1.75 2 3; do
     HYDRO=null
     
     EXPERIMENT=${CLIMATE}_${TYPE}_e_${E}_sia_hydro_${HYDRO}
