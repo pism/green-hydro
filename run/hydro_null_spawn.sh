ab97729e7528b455aa91943938aa3a4e96d57deb
Andy Aschwanden <aaschwanden@alaska.edu>
2014-10-03 13:22:28 -0800

    added missing job numbers

--- run/hydro_null_spawn.sh
+++ run/hydro_null_spawn.sh
@@ -181,7 +181,7 @@ for E in 1.25; do
                 OUTFILE=g${GRID}m_${EXPERIMENT}_${DURA2}a.nc
                 
                 cmd="PISM_DO="" PISM_OFORMAT=$OFORMAT REGRIDFILE=$REGRIDFILE PISM_DATANAME=$PISM_DATANAME TSSTEP=daily EXSTEP=yearly PARAM_FTT=foo REGRIDVARS=litho_temp,enthalpy,tillwat,bmelt,Href PARAM_SIAE=$E PARAM_PPQ=$PPQ PARAM_TEFO=$TEFO PARAM_TTPHI=$PARAM_TTPHI PARAM_SSA_N=$SSA_N ./run.sh $NN $CLIMATE $DURA $GRID hybrid $HYDRO $OUTFILE $INFILE"
-                echo "$cmd 2>&1 | tee job_1.\${PBS_JOBID}" >> $SCRIPT                            
+                echo "$cmd 2>&1 | tee job_2.\${PBS_JOBID}" >> $SCRIPT                            
                 echo >> $SCRIPT
 
                 echo "# $SCRIPT written"
@@ -232,7 +232,7 @@ for E in 1.25; do
                 OUTFILE=g${GRID}m_${EXPERIMENT}_${DURA2}a.nc
                 
                 cmd="PISM_DO="" PISM_OFORMAT=$OFORMAT REGRIDFILE=$REGRIDFILE PISM_DATANAME=$PISM_DATANAME TSSTEP=daily EXSTEP=yearly PARAM_FTT=foo REGRIDVARS=litho_temp,enthalpy,tillwat,bmelt,Href PARAM_SIAE=$E PARAM_PPQ=$PPQ PARAM_TEFO=$TEFO PARAM_TTPHI=$PARAM_TTPHI PARAM_SSA_N=$SSA_N ./run.sh $NN $CLIMATE $DURA $GRID hybrid $HYDRO $OUTFILE $INFILE"
-                echo "$cmd 2>&1 | tee job_1.\${PBS_JOBID}" >> $SCRIPT                            
+                echo "$cmd 2>&1 | tee job_2.\${PBS_JOBID}" >> $SCRIPT                            
                 echo >> $SCRIPT
 
                 echo "# $SCRIPT written"
