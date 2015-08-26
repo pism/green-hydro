481e44312950048f57a62a276f2a39c9b1bd21c0
Andy Aschwanden <aaschwanden@alaska.edu>
2015-08-26 13:30:49 -0800

    more cleaning

--- run/hirham_hindcast.sh
+++ run/hirham_hindcast.sh
@@ -245,7 +245,7 @@ SUBMIT=submit_hirham_hindcast.sh
 rm -f $SUBMIT
 cat - > $SUBMIT <<EOF
 $SHEBANGLINE
-for FILE in hirham_hindcast_${STARTYEAR}_${ENDYEAR}_g${GRID}m_*_run.sh; do
+for FILE in hirham_hindcast_${STARTYEAR}_${ENDYEAR}_g${GRID}m_${CLIMATE}_v${VERSION}_${TYPE}_${RELAXYEARS}a_ssa_e_*_k_${K}_calving_${CALVING}_*_ocean_*_run; do
   JOBID=\$(qsub \$FILE)
   fbname=\$(basename "\$FILE" _run.sh)
   POSTR=\${fbname}_relax_post.sh
