4fc6cd73bddac5976660e93de927ec06bb573928
Andy Aschwanden <aaschwanden@alaska.edu>
2015-09-02 10:17:28 -0800

    fixed a typo

--- run/hirham_hindcast.sh
+++ run/hirham_hindcast.sh
@@ -178,8 +178,8 @@ for SSA_E in 0.8; do
                 PISM_TIMEFILE=GR6b_ERAI_1989_2011_4800M_BIL_MM.nc
                 PISM_SURFACE_BCFILE=GR6b_ERAI_1989_2011_4800M_BIL_MM.nc
                 PISM_OCEAN_BCFILE=ocean_forcing_${GRID}m_1989-2011_v${VERSION}_${TYPE}_${OTYPE}.nc
-                EXPERIMENTH=hindcast_${STARTYEAR}_${ENDYEAR}_${CLIMATE}_v${VERSION}_${TYPE}_ssa_e_${SSA_E}_k_${K}_calving_${CALVING}_${THK}_ocean_${OTYPE}
-                RUNE=v${VERSION}_${TYPE}_${RELAXYEARS}a_ssa_e_${SSA_E}_k_${K}_calving_${CALVING}_${THK}_ocean_${OTYPE}
+                EXPERIMENTH=hindcast_${STARTYEAR}_${ENDYEAR}_${CLIMATEH}_v${VERSION}_${TYPE}_ssa_e_${SSA_E}_k_${K}_calving_${CALVING}_${THK}_ocean_${OTYPE}
+                RUNE=v${VERSION}_${TYPE}_ssa_e_${SSA_E}_k_${K}_calving_${CALVING}_${THK}_ocean_${OTYPE}
                 SCRIPT=hirham_hindcast_${STARTYEAR}_${ENDYEAR}_g${GRID}m_${RUNE}_run.sh
                 POSTH=hirham_hindcast_${STARTYEAR}_${ENDYEAR}_g${GRID}m_${RUNE}_hindcast_post.sh
 
